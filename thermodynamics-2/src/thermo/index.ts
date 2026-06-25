a2868feda548302fb36e74634bad37fdc33291f5
Jayson E. Pantollana <129268177+jaysonpantollana@users.noreply.github.com>
2026-06-25 12:36:45 +0800

    clean up repo: remove .vscode, thermodynamics-2, package-lock.json and add .gitignore

--- thermodynamics-2/src/thermo/index.ts
+++
@@ -1,5 +0,0 @@
-export {default as Scene3D} from './Scene3D';
-export {default as Sidebar} from './Sidebar';
-export {default as InfoPanel} from './InfoPanel';
-export {default as Controls} from './Controls';
-export {componentData} from './data';
